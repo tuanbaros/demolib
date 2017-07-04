22424461a2b9204f3933801f47736d6fd671bc9f
tuannt <tuanbaros@yahoo.com.vn>
2017-07-04 08:21:18 +0700

    init

---
+++ awesomelib/src/main/java/com/simple/awesomelib/ToastUtil.java
@@ -0,0 +1,14 @@
+package com.simple.awesomelib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by FRAMGIA\nguyen.thanh.tuan on 04/07/2017.
+ */
+
+public class ToastUtil {
+    public Toast toast(Context context, String content) {
+        return Toast.makeText(context, content, Toast.LENGTH_SHORT);
+    }
+}
